89f7990d0d5805f16043ade96a9818d9168bb8c9
Moonshade <enigmatic.gate@gmail.com>
2014-12-13 15:49:47 +0500

    increased difference between columns in visual forecast

--- src/js/controllers/visualController.js
+++ src/js/controllers/visualController.js
@@ -4,15 +4,18 @@ angular.module('weather')
 		var colorSelector = new ColorSelector();
 		var temps = [];
 		const MIN_COLUMN_HEIGHT = 14;
+		const DEFAULT_COLUMN_MAX = 20;
 		$scope.appState.changeTab('visual');
 		$scope.gistData = $scope.appState.Forecast.days.slice(0,8);
 		utils.forEach($scope.gistData, function(key, element) {
 			temps.push(element.parts.day_short.temp.avg);
 		});
 		var minTemp = Math.min.apply(Math, temps);
+		var maxTemp = Math.max.apply(Math, temps);
+		var tempStep = Math.floor(DEFAULT_COLUMN_MAX / (maxTemp - minTemp));
 		utils.forEach($scope.gistData, function(key, element) {
 			var temperature = element.parts.day_short.temp.avg;
-			element.height = (MIN_COLUMN_HEIGHT + temperature - minTemp) + 'vw';
+			element.height = (MIN_COLUMN_HEIGHT + (temperature - minTemp) * tempStep) + 'vw';
 			element.delimiterColor = colorSelector.darken(colorSelector.tempToColor(temperature), 25);
 		});
 	}]);
